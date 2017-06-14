e8c33bed6992ced4c77c88f1e585d045d5dbee2d
Matias Camiletti <matias.camiletti@gmail.com>
2017-06-14 00:05:52 -0300

    Primer commit.

---
+++ src/angular-mia-push.js
@@ -0,0 +1,48 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('mobileiaPush', ['mobileiaAuthentication'])
+        .factory('mobileiaPush', mobileiaPush);
+
+    mobileiaPush.$inject = ['mobileiaAuth', '$rootScope'];
+
+    function mobileiaPush(mobileiaAuth, $rootScope) {
+        var socket = null;
+        var isConnected = false;
+        var baseUrl = 'http://localhost:8080/';
+        
+        var service = {
+            init: init,
+            on: on,
+            emit: emit
+        };
+        
+        return service;
+        
+        function init(){
+            // Verificamos si ya se inicio un socket
+            if(isConnected){
+                console.log("Ya existe un socket abierto");
+                return false;
+            }
+            // Iniciamos socket
+            socket = io.connect(baseUrl, {query: 'appId=' + mobileiaAuth.getAppId() + '&accessToken=' + mobileiaAuth.getAccessToken()});
+            // Seteamos variable que se inicio socket
+            isConnected = true;
+        };
+        
+        function on(event, callback){
+            socket.on(event, function(msg){
+                $rootScope.$apply(function () {
+                    callback(msg);
+                });
+            });
+        };
+        
+        function emit(event, message, callback){
+            socket.emit('miapush_event', {event: event, message: message}, callback);
+        };
+
+    }
+})();
